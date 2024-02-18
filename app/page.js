512d10e7947d006ee86eeb76f0ad88e585673265
Oliulla <razasm29@gmail.com>
2024-02-18 19:39:19 +0600

    :fire: Remove Test Code

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-"use client"
-import { Button } from "@material-tailwind/react";
-
 export default function Home() {
   return (
     <main className="">
-      <Button>Button from material tailwind</Button>
     </main>
   );
 }
