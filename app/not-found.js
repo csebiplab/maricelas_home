2e39126f1531fc3c791356e76cdc9db7335b48c0
Oliulla <razasm29@gmail.com>
2024-02-29 00:07:47 +0600

    :sparkles: Added custom notfound page

---
+++ app/not-found.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from '@material-tailwind/react'
+import Link from 'next/link'
+import { Suspense } from 'react'
+
+export default function NotFound() {
+    return (
+        <Suspense>
+            <div className='w-full mx-auto flex justify-center items-center h-screen'>
+                <div className='text-center'>
+                    <h1>Not found – 404!</h1>
+                    <div>
+                        <Link href="/">
+                            <Button className='bg-cyan-50 hover:bg-cyan-500'>Go back to Home</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </Suspense>)
+
+}
