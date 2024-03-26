ac846a974afb921f376f92ab23b187f25f703004
Tawhidul3233 <104293821+Tawhidul3233@users.noreply.github.com>
2024-03-26 20:50:28 +0600

    footer design done

--- components/__layouts/Footers/Footer.jsx
+++ components/__layouts/Footers/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
 
   return (
     <div className="">
-      <div className="footer__bg bg-blend-multiply bg-[#173f3f] lg:px-[35px] xs:px-[20px] !text-white">
+      <div className="footer__bg bg-blend-multiply bg-[#54042C] lg:px-[35px] xs:px-[20px] !text-white">
         <section>
           <footer
             className="z-50 2xl:pt-[95px] lg:pt-[70px] pt-[50px] text-white "
@@ -49,14 +49,22 @@ const Footer = () => {
               <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-7 gap-10 ">
 
                 <div className=" lg:col-span-2 md:col-span-1 items-center">
-                  <div className="lg:h-[128px] xs:h-[76px] lg:w-[128px] xs:w-[76px]">
+                  {/* <div className="lg:h-[128px] xs:h-[76px] lg:w-[128px] xs:w-[76px]">
                     <Image src={LOGO} height={"128px"} width={"128px"} alt="" className="" />
+
+                  </div> */}
+                  <div>
+                    <h4 className=" lg:text-[25px] text-[20px]">We Are Maricela's Cleaning Magnificence</h4>
+                    <p className="text-white lg:pt-6 pt-2 mb-2 ml-2 lg:text-[13px] xs:text-[10px] ">
+
+                      We work with a passion of taking challenges and creating new ones in advertising sector.
+                    </p>
                   </div>
-                  <p className="text-white lg:pt-6 pt-2 mb-2 ml-2 lg:text-[13px] xs:text-[10px] ">
-                    Easy reno simplifies the process and costs of renovation by
-                    bringing design, sourcing and management under one roof.
-                  </p>
-                  {/* <h5 className="text-gray-500">Customer Reviews</h5> */}
+
+                  <div className="lg:mt-[35px]">
+                    <h4 className=" lg:text-[25px] text-[20px]">Open Hours:</h4>
+                    <p>Mon – Sat: 8 am – 5 pm, <br />Sunday: CLOSED</p>
+                  </div>
 
                   <div className=" flex gap-8 mt-8">
                     <FaFacebook className=" lg:w-[30px] lg:h-[28px] w-auto h-auto" />
@@ -68,6 +76,7 @@ const Footer = () => {
 
                 <div className=" lg:col-span-5 md:col-span-2 grid sm:grid-cols-7 xs:grid-cols-5 lg:gap-10 gap-3">
 
+                  {/* news line */}
                   <div className="sm:col-span-2 xs:col-span-1 ">
                     <h3 className="font-semibold text-white  lg:text-[20px]  xs:text-[15px] lg:mb-10 md:mb-5 mb-3 ">
                       {" "}
@@ -122,6 +131,7 @@ const Footer = () => {
                     </ul>
                   </div>
 
+                  {/* services line */}
                   <div className=" sm:col-span-2 xs:col-span-2 ">
                     <h3 className="font-semibold text-white lg:text-[20px] xs:text-[15px]  lg:mb-10 md:mb-5 mb-3 ">
                       Ours Services
@@ -133,7 +143,7 @@ const Footer = () => {
                           className="footer-link lg:text-[16px]  xs:text-[8px]  items-center text-white"
                         >
                           <MdKeyboardDoubleArrowRight />
-                          Vinyl pool
+                          House Cleaning
                         </Link>
                       </li>
                       <li className="  lg:py-8 xs:py-[12px]">
@@ -142,7 +152,7 @@ const Footer = () => {
                           className="footer-link lg:text-[16px] xs:text-[8px]  items-center text-white"
                         >
                           <MdKeyboardDoubleArrowRight />
-                          Fiberglass pool
+                          Office Cleaning
                         </Link>
                       </li>
                       <li className=" ">
@@ -151,7 +161,7 @@ const Footer = () => {
                           className="footer-link lg:text-[16px]  xs:text-[8px]  items-center text-white"
                         >
                           <MdKeyboardDoubleArrowRight />
-                          Concrete pool
+                          Kitchen Cleaning
                         </Link>
                       </li>
                       <li className=" lg:py-8 xs:py-[12px]">
@@ -160,7 +170,7 @@ const Footer = () => {
                           className="footer-link lg:text-[16px]  xs:text-[8px]  items-center  text-white"
                         >
                           <MdKeyboardDoubleArrowRight />
-                          ICF pool
+                          Water Tank Cleaning
                         </Link>
                       </li>
                       <li className=" ">
@@ -169,12 +179,22 @@ const Footer = () => {
                           className="footer-link lg:text-[16px]  xs:text-[8px]  items-center text-white"
                         >
                           <MdKeyboardDoubleArrowRight />
-                          Pool Installations
+                          Window Cleaning
+                        </Link>
+                      </li>
+                      <li className=" ">
+                        <Link
+                          href="#"
+                          className="footer-link lg:text-[16px]  xs:text-[8px]  items-center text-white"
+                        >
+                          <MdKeyboardDoubleArrowRight />
+                          Plumbing Service
                         </Link>
                       </li>
                     </ul>
                   </div>
 
+                  {/* contact line  */}
                   <div className=" sm:col-span-3 xs:col-span-2 items-center">
                     <h3 className="font-semibold text-white   lg:text-[20px] xs:text-[15px] lg:mb-10 md:mb-5 mb-3">
                       Contact Us
@@ -183,10 +203,10 @@ const Footer = () => {
                       <li>
                         <a
                           href=""
-                          className="footer-link items-center lg:text-[16px]  xs:text-[8px]  text-white"
+                          className="footer-link flex gap-4 items-center lg:text-[16px]  xs:text-[8px]  text-white"
                         >
                           <FaMapMarkedAlt className=" lg:h-7 lg:w-7  h-4 w-4" />{" "}
-                          1331 Major Mackenzie dr West, Vaughan
+                          13809 Hollow Canyon Lane Rosharon, TX 77583
                         </a>
                       </li>
                       <li className=" lg:py-8 xs:py-[12px]">
@@ -195,7 +215,7 @@ const Footer = () => {
                           className="footer-link items-center lg:text-[16px] xs:text-[8px]  text-white"
                         >
                           <FaPhoneVolume className=" lg:h-7 lg:w-7 h-4 w-4" />{" "}
-                          +1(647)449-9512
+                          (832)8204035
                         </a>
                       </li>
                       <li>
@@ -204,11 +224,11 @@ const Footer = () => {
                           className="footer-link items-center lg:text-[16px] xs:text-[8px]  text-white"
                         >
                           <IoIosMail className=" lg:h-7 lg:w-7 h-4 w-4" />{" "}
-                          easypools.landscaping@gmail.com
+                          maricela.maldonado@maricelasmagnificence.com
                         </a>
                       </li>
                       <div>
-                        <div className=" xs:hidden md:block ">
+                        <div className=" hidden md:block ">
                           <div className="grid  md:grid-cols-3 lg:gap-8 gap-5 lg:my-12">
                             {footerIcon.map((fIcon, i) => (
                               <div key={i} className=" col-span-1">
@@ -227,7 +247,7 @@ const Footer = () => {
                 </div>
 
                 {/* This section show when screen sm or xs */}
-                <div className=" xs:block md:hidden items-center">
+                <div className=" block md:hidden items-center">
                   <div>
                     <div className=" gap-5  xs:flex xs:justify-between xs:items-center xs:mb-8 ">
                       {footerIcon.map((fIcon, i) => (
@@ -251,23 +271,23 @@ const Footer = () => {
 
       {/* creater company credit */}
       <div className="  footer-bottom-bg    ">
-        <section className=" grid lg:grid-cols-7 gap-5 lg:py-6 xs:py-2   items-center  md:mx-auto text-center ">
-          <div className=" lg:col-span-3 flex items-center xs:justify-center lg:justify-normal gap-2  ">
-            <p className=" lg:text-[16px] md:text-sm xs:text-[8px]  ">
+        <section className=" grid lg:grid-cols-8 gap-5 lg:py-6 xs:py-2   items-center  md:mx-auto text-center ">
+          <div className=" lg:col-span-3 flex items-center justify-center lg:justify-normal gap-2  ">
+            <p className=" lg:text-[16px] md:text-sm text-[8px]  ">
               {" "}
               <b className="text-white">WEBSITE DESIGN AND SEO BY :-</b>{" "}
             </p>
-            <Image src={gokundo} alt="gokundo" width={"160px"} height={"40px"} className=" ml-2 " />
+            <Image src={gokundo} alt="gokundo" width={150} height={30} className=" ml-2 " />
           </div>
-          <div className=" lg:col-span-2 lg:block xs:hidden ">
+          <div className=" lg:col-span-3 lg:block hidden ">
             <p className=" 2xl:text-[16px] lg:text-[14px] text-[12px]  text-white">
-              © All Copyright 2024 by Easy Pools
+              © All Copyright 2024 by Maricela's Cleaning Magnificence
             </p>
           </div>
-          <div className=" lg:col-span-2  lg:block xs:hidden">
-            <ul className=" lg:text-[16px] md:text-sm  flex justify-evenly  ">
-              <li className="text-white"> Terms & Condition </li>
-              <li className="text-white"> Privacy Policy </li>
+          <div className=" lg:col-span-2  lg:block hidden">
+            <ul className=" lg:text-[16px] text-xs  flex justify-evenly  ">
+              <li className="text-white  "> Terms & Condition </li>
+              <li className="text-white  "> Privacy Policy </li>
             </ul>
           </div>
         </section>
