1d9fdfd6ee337707282c8234856a48ba74fa7477
tonmoy <tonmoy3673@gmail.com>
2024-03-28 00:06:59 +0600

    create cost for service section

--- components/__shared_one_time/HomeSections/CostForServicesSection.jsx
+++ components/__shared_one_time/HomeSections/CostForServicesSection.jsx
@@ -1,7 +1,21 @@
-import React from "react";
 
 const CostForServicesSection = () => {
-  return <div>CostForServicesSection CostForServicesSection</div>;
+  return <div className="lg:px-[35px] xs:px-[20px]">
+    <p className="text-sm md:text-base text-[#1F1F1F]">Cost For Services</p>
+      <div className="flex gap-2 md:gap-3 lg:gap-[27px]">
+      <div className="md:text-start text-center">
+        
+        <h2 className="text-base lg:text-[32px] font-semibold mt-2 md:mt-[12px] text-[#1F1F1F] lg:leading-normal">How Much Does Commercial & Residential Cleaning Service Cost</h2>
+        <p className="text-xs lg:text-base mt-2 lg:mt-[15px] lg:leading-[32px]">The cost of commercial & residential cleaning services varies depending on various factors such as the size of the space, the frequency of cleaning, and the specific services required. Generally, commercial cleaning services tend to be more expensive than residential ones due to the larger areas and specialized equipment often involved. 
+Ultimately, the best way to determine the cost of cleaning services for either commercial or residential properties is to request quotes from multiple cleaning websites and compare their services and pricing structures. Additionally, considering factors like the reputation of the company, their level of experience, and the quality of their work can help ensure that you get the best value for your money.</p>
+      </div>
+      {/* ================ Images card section ========== */}
+      <div>
+        <h3>Images</h3>
+      </div>
+      </div>
+
+  </div>;
 };
 
 export default CostForServicesSection;
