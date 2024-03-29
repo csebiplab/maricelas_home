2621ef4d56abc1c714df243fa3fd3e7083598cf1
Oliulla <razasm29@gmail.com>
2024-03-29 22:12:13 +0600

    :hammer: Done about us section responsive

--- components/__shared_one_time/HomeSections/AboutUsSection.jsx
+++ components/__shared_one_time/HomeSections/AboutUsSection.jsx
@@ -4,52 +4,65 @@ import person3 from "@/public/assets/images/person3.png";
 import Heading from "@/ui/Heading";
 import Image from "next/image";
 const AboutUsSection = () => {
-  return <div className="custom-container">
-    <div className="flex gap-2 md:gap-3 3xl:gap-[56px] lg:gap-[30px] 3xl:py-[120px] lg:py-[100px] py-[60px]">
-      {/* ============= Images Section Start ========== */}
-      <div className="flex gap-2 lg:gap-[14px]">
-        {/* ============ Images one ========= */}
-        <div>
-        <Image
-                src={person1}
-                width={320}
-                height={372}
-                alt="person1"
-                className="w-full"
-              />
-        </div>
-        
-              
-        {/* ================ Images Two ============ */}
-        <div>
-        <Image
-                src={person2}
-                width={320}
-                height={135.85}
-                alt="person2"
-                className="w-full"
-              />
-              <Image
-                src={person3}
-                width={320}
-                height={222.16}
-                alt="person3"
-                className="w-full mt-2 lg:mt-[13px]"
-              />
-        </div>
-      </div>
-      {/* ============== Images Section End =========== */}
+  return (
+    <div className="custom-container">
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start gap-2 md:gap-3 3xl:gap-[56px] lg:gap-[30px] 3xl:py-[120px] lg:py-[100px] py-[60px]">
+        {/* ============= Images Section Start ========== */}
+        <div className="flex gap-2 lg:gap-[14px]">
+          {/* ============ Images one ========= */}
+          <div className="w-full h-full hidden lg:block">
+            <Image
+              src={person1}
+              width={320}
+              height={372}
+              alt="person1"
+              className="!max-w-[320px] !max-h-[372px]"
+            />
+          </div>
 
-     {/* ============= Text Section ============== */}
-    <div className="md:text-start text-center md:w-1/2">
-    <Heading text={'About Us'} />
-        <h2 className="2xl:text-[32px] md:text-[20px] text-[18px] md:pb-0 pb-[20px] font-semibold text-[#1F1F1F]">Welcome to Our Commercial & Residential Cleaning Services in Houston</h2>
-        <p className="text-xs lg:text-base mt-2 lg:mt-[15px] lg:leading-[32px]">Welcome to Maricela's Cleaning Magnificence, a company of commercial & residential cleaning services in Houston. Our current focus is on catering to both commercial and residential clients in Houston, Texas. We aim to provide our clients with comprehensive and high-quality services that meet their specific needs and requirements. Our team is composed of skilled professionals with extensive experience in the industry, and we are committed to delivering exceptional results that exceed our client’s expectations.</p>
+          {/* ================ Images Two ============ */}
+          <div className="w-full h-full !lg:w-[320px] !lg:h-[222px]">
+            <Image
+              src={person2}
+              width={320}
+              height={135.85}
+              alt="person2"
+              className="w-full hidden lg:block"
+            />
+            <Image
+              src={person3}
+              width={320}
+              height={222}
+              alt="person3"
+              className="h-full lg:max-h-[222px] w-[500px] !lg:w-[320px] !lg:h-[222px] mt-2 lg:mt-[13px] border border-red-700"
+            />
+          </div>
+        </div>
+        {/* ============== Images Section End =========== */}
+
+        {/* ============= Text Section ============== */}
+        <div className="text-center xl:text-start lg:w-1/2 mt-5 lg:mt-0">
+          <div className="flex justify-center lg:justify-start">
+            <Heading text={"About Us"} />
+          </div>
+          <h2 className="text-sm md:text-base lg:text-2xl 2xl:text-[32px] font-semibold text-[#1F1F1F]">
+            Welcome to Our Commercial & Residential Cleaning Services in Houston
+          </h2>
+          <p className="text-xs lg:text-base mt-[15px] lg:mt-[25px] lg:leading-[32px]">
+            Welcome to Maricela's Cleaning Magnificence, a company of commercial
+            & residential cleaning services in Houston. Our current focus is on
+            catering to both commercial and residential clients in Houston,
+            Texas. We aim to provide our clients with comprehensive and
+            high-quality services that meet their specific needs and
+            requirements. Our team is composed of skilled professionals with
+            extensive experience in the industry, and we are committed to
+            delivering exceptional results that exceed our client’s
+            expectations.
+          </p>
+        </div>
       </div>
-      
     </div>
- 
-  </div>;
+  );
 };
 
 export default AboutUsSection;
