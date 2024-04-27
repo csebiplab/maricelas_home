db10b86149aa869ecb70ccfedc140aa19c8fa8bf
tonmoy <tonmoy3673@gmail.com>
2024-04-27 21:28:07 +0600

    added slider card

--- components/__shared_one_time/HomeSections/Approach/DifferentApproachForWorkSection.jsx
+++ components/__shared_one_time/HomeSections/Approach/DifferentApproachForWorkSection.jsx
@@ -5,6 +5,7 @@ import app2 from "@/public/assets/images/app2.png";
 import app3 from "@/public/assets/images/app3.png";
 import Image from "next/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/effect-coverflow";
@@ -43,6 +44,21 @@ const slideData = [
     imgSrc: app3,
     title: 'Trained and Professional Staff',
     description: `Our team consists of highly trained and professional cleaners who are committed to delivering exceptional service. They undergo rigorous training and adhere to strict quality standards to ensure that every job is completed to perfection.`,
+  },
+  {
+    imgSrc: app1,
+    title: 'Customized Cleaning Plans',
+    description: `At our commercial & residential cleaning service in Houston, TX, we believe in tailoring our approach to meet the unique needs of each client. We don't offer a one-size-fits-all solution; instead, we work closely with our customers to understand their specific requirements and preferences.`,
+  },
+  {
+    imgSrc: app2,
+    title: 'Advanced Cleaning Techniques',
+    description: `We stay ahead of the curve by utilizing the latest cleaning technologies and techniques. From eco-friendly products to advanced equipment, we ensure that our cleaning methods are not only effective but also environmentally conscious. We stay ahead of the curve by utilizing.`,
+  },
+  {
+    imgSrc: app3,
+    title: 'Trained and Professional Staff',
+    description: `Our team consists of highly trained and professional cleaners who are committed to delivering exceptional service. They undergo rigorous training and adhere to strict quality standards to ensure that every job is completed to perfection.`,
   }
   
 
@@ -61,18 +77,19 @@ const DifferentApproachForWorkSection = () => {
               <h2 className="text-sm md:text-base lg:text-2xl 2xl:text-3xl font-semibold text-[#1F1F1F] mt-[15px]">
                 Our Approach To Cleaning Service Is Different
               </h2>
-              <p className="text-xs lg:text-base mt-2 md:mt-4 mb-3 lg:mb-5 lg:leading-[32px]">
+              <p className="text-xs lg:text-base mt-2 md:mt-4 mb-5 lg:leading-[32px]">
                 Welcome to a cleaning service experience like no other. At
                 Maricela's Cleaning Magnificence, our approach to cleaning
                 transcends the ordinary.
               </p>
               <div className="hidden lg:flex lg:gap-5 md:gap-3 md:justify-start justify-center mt-2 md:mt-4">
                 <button className="p-3 rounded-full border-2  hover:text-white hover:bg-[#880769]">
-                  <FaChevronLeft fill="#880769" />
+                  <FaChevronLeft className="fill-[#880769] hover:fill-white" />
                 </button>
                 <button className=" hover:text-black hover:bg-white border-2 p-3 bg-[#880769] rounded-full">
-                  <FaChevronRight fill="#ffffff" />
+                  <FaChevronRight className='hover:fill-[#880769] fill-white' />
                 </button>
+                {/* <SwiperNavButtons/> */}
               </div>
             </div>
           </div>
@@ -108,7 +125,7 @@ const DifferentApproachForWorkSection = () => {
                 <h4 className="text-base py-1 md:py-2">
                  {slideInfo?.title}
                 </h4>
-                <p className="lg:text-sm text-xs md:px-1 text-[#252525] leading-[25px]">
+                <p className="lg:text-sm mt-1 md:mt-0 text-xs md:px-1 text-[#252525] leading-[25px]">
                  {slideInfo?.description}
                 </p>
               </div>
