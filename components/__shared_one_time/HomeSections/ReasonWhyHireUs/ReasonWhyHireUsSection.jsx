fd55aad6accdc33d6ccb72ab3d7acbf2ec505dec
Oliulla <razasm29@gmail.com>
2024-05-10 22:20:18 +0600

    Added swipper

--- components/__shared_one_time/HomeSections/ReasonWhyHireUs/ReasonWhyHireUsSection.jsx
+++ components/__shared_one_time/HomeSections/ReasonWhyHireUs/ReasonWhyHireUsSection.jsx
@@ -7,7 +7,40 @@ import jharu from "../../../../public/assets/benefits/jharu.png";
 import leaf from "../../../../public/assets/benefits/leaf.png";
 import like from "../../../../public/assets/benefits/like.png";
 import lisen from "../../../../public/assets/benefits/lisen.png";
+
+import { Navigation, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "swiper/css/virtual";
+
 import "./ReasonWhyHireUs.css";
+
+const breakpoints = {
+  0: {
+    slidesPerView: 1,
+    spaceBetween: 30,
+  },
+  560: {
+    slidesPerView: 2,
+    spaceBetween: 20,
+  },
+  768: {
+    slidesPerView: 3,
+    spaceBetween: 20,
+  },
+  1024: {
+    slidesPerView: 4,
+    spaceBetween: 20,
+  },
+  1440: {
+    slidesPerView: 5,
+    spaceBetween: 20,
+  },
+};
 
 const reasons = [
   {
@@ -55,84 +88,97 @@ const ReasonWhyHireUsSection = () => {
             </h2>
           </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 4xl:grid-cols-5 gap-3">
+        {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 4xl:grid-cols-5 gap-3"> */}
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          breakpoints={breakpoints}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            pauseOnMouseEnter: false,
+            disableOnInteraction: false,
+            stopOnLastSlide: false,
+          }}
+          speed={3000}
+          allowTouchMove={false}
+        >
           {reasons?.map(({ title, img, details }, index) => (
-            <div
-              key={index}
-              className="w-full h-full md:w-[260px] md:h-[190px] bg-white rounded-md py-[11px] relative"
-            >
-              <div className="absolute top-0 left-0 z-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="65"
-                  height="49"
-                  viewBox="0 0 65 49"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="24.5164"
-                    cy="-10.4439"
-                    rx="40.1786"
-                    ry="40.3041"
-                    fill="#880769"
-                    fillOpacity="0.45"
-                  />
-                  <ellipse
-                    cx="-7.62613"
-                    cy="8.09586"
-                    rx="40.1786"
-                    ry="40.3041"
-                    fill="#54042C"
-                    fillOpacity="0.35"
-                  />
-                </svg>
-              </div>
-              <div className="absolute top-0 right-0 z-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="68"
-                  height="55"
-                  viewBox="0 0 68 55"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="74.1961"
-                    cy="14.5443"
-                    rx="40.1786"
-                    ry="40.3041"
-                    fill="#880769"
-                    fillOpacity="0.45"
-                  />
-                  <ellipse
-                    cx="40.4461"
-                    cy="-14.4742"
-                    rx="40.1786"
-                    ry="40.3041"
-                    fill="#54042C"
-                    fillOpacity="0.35"
-                  />
-                </svg>
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <div className="flex justify-center">
-                  <div className="bg-primary w-[73px] h-[73px] rounded-full flex justify-center items-center">
-                    <Image
-                      src={img}
-                      alt="Icon"
-                      width={40}
-                      height={40}
-                      className="w-10 h-10"
+            <SwiperSlide key={index}>
+              <div className="w-full h-full md:w-[260px] md:h-[220px] bg-white rounded-md py-[11px] relative">
+                <div className="absolute top-0 left-0 z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="65"
+                    height="49"
+                    viewBox="0 0 65 49"
+                    fill="none"
+                  >
+                    <ellipse
+                      cx="24.5164"
+                      cy="-10.4439"
+                      rx="40.1786"
+                      ry="40.3041"
+                      fill="#880769"
+                      fillOpacity="0.45"
                     />
-                  </div>
+                    <ellipse
+                      cx="-7.62613"
+                      cy="8.09586"
+                      rx="40.1786"
+                      ry="40.3041"
+                      fill="#54042C"
+                      fillOpacity="0.35"
+                    />
+                  </svg>
                 </div>
-                <h5 className="mt-3 mb-1 !text-base font-bold text-center">
-                  {title}
-                </h5>
-                <p className="px-2 text-sm text-center">{details}</p>
+                <div className="absolute top-0 right-0 z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="68"
+                    height="55"
+                    viewBox="0 0 68 55"
+                    fill="none"
+                  >
+                    <ellipse
+                      cx="74.1961"
+                      cy="14.5443"
+                      rx="40.1786"
+                      ry="40.3041"
+                      fill="#880769"
+                      fillOpacity="0.45"
+                    />
+                    <ellipse
+                      cx="40.4461"
+                      cy="-14.4742"
+                      rx="40.1786"
+                      ry="40.3041"
+                      fill="#54042C"
+                      fillOpacity="0.35"
+                    />
+                  </svg>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="flex justify-center">
+                    <div className="bg-primary w-[73px] h-[73px] rounded-full flex justify-center items-center">
+                      <Image
+                        src={img}
+                        alt="Icon"
+                        width={40}
+                        height={40}
+                        className="w-10 h-10"
+                      />
+                    </div>
+                  </div>
+                  <h5 className="mt-3 mb-1 !text-base font-bold text-center">
+                    {title}
+                  </h5>
+                  <p className="px-2 text-sm text-center">{details}</p>
+                </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
+        {/* </div> */}
       </div>
     </div>
   );
