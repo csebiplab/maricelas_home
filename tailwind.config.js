c0b67fc1018bb047356ad89b57b22a4c0b921d72
Oliulla <razasm29@gmail.com>
2024-06-05 00:47:59 +0600

    fixed all err

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = withMT({
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,41 +12,29 @@ module.exports = withMT({
     ],
     darkMode: ["class"],
     theme: {
-        screens: {
-            // => beshi choto screen mobile 639-280px
-            // => moddhom screen mobile 767-640px
-            'xs': '280px',
-
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-            // => tablet screen 1023px-768px
-
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-
-            // => mid level desktop screen 1279-1024px
-
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-
-            // => Extra desktop screen 1439px-1280px
-
-            '2xl': '1440px',
-            // => @media (min-width: 1536px) { ... }
-
-            // 2(dui gun extra screen) ==> 1535px-1440px
-
-            '3xl': '1536px',
-
-            '4xl': '1736px',
-
-            '5xl': '1920px',
-        },
-
         extend: {
+            screens: {
+                'xs': '280px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1440px',
+                '3xl': '1536px',
+                '4xl': '1736px',
+                '5xl': '1920px',
+            },
+            maxWidth: {
+                'screen-5xl': '1920px',
+            },
+            padding: {
+                '3xl': '10rem',
+                '4xl': '12rem',
+                '5xl': '16rem',
+            },
+            spacing: {
+                '285': '285px',
+            },
             colors: {
                 primary: {
                     DEFAULT: "#880769",
@@ -77,48 +66,24 @@ module.exports = withMT({
                 danger: {
                     DEFAULT: "#DC0000",
                 },
-                dark: {
-                    DEFAULT: "#000000",
-                    50: "#111111",
-                    100: "#1B1B1B",
-                    200: "#1F1F1F",
-                    300: "#535455",
-                    400: "#333333",
-                    500: "#040404",
-                },
                 gray: {
                     DEFAULT: "#D9D9D9",
                 },
+                'gray-1': "var(--gray-1)",
+                'gray-2': "var(--gray-2)",
+                'gray-3': "var(--gray-3)",
+                'background': "var(--background-color)",
+                'foreground': "var(--foreground)",
             },
-            spacing: {
-                '24': "6.5rem",
-                "1": "2px"
-            },
-
-            "gray-1": "var(--gray-1)",
-            "gray-2": "var(--gray-2)",
-            "gray-3": "var(--gray-3)",
-            background: "var(--background-color)",
-            foreground: "var(--foreground)",
-
-            tremor: {
-                background: {
-                    DEFAULT: "var(--background-color)",
-                },
-                border: {
-                    DEFAULT: "var(--gray-2)",
-                },
-                ring: {
-                    DEFAULT: "var(--gray-2)",
-                },
-                content: {
-                    subtle: "var(--gray-2)",
-                    DEFAULT: "var(--gray-1)",
-                    emphasis: "var(--foreground)",
-                },
-            },
+            backgroundColor: theme => ({
+                ...theme('colors'),
+                'background': 'var(--background-color)',
+            }),
+            textColor: theme => ({
+                ...theme('colors'),
+                'foreground': 'var(--foreground)',
+            }),
         },
-
     },
     plugins: [],
 });
