e44f3256d5e2b50c774b94c11398b5b9f596f82b
Oliulla <razasm29@gmail.com>
2024-08-03 00:25:14 +0600

    off  sitemap and robot

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,25 +1,36 @@
-import dbConnect from "@/lib/db.connect";
-import siteMap from "@/models/siteMap";
+// import dbConnect from "@/lib/db.connect";
+// import siteMap from "@/models/siteMap";
 
-export default async function sitemap() {
-    await dbConnect();
-    const data = await siteMap.find({});
+// export default async function sitemap() {
+//     await dbConnect();
+//     const data = await siteMap.find({});
 
-    if (data?.length < 1) {
-        return [{
-            url: `${process.env.NEXT_PUBLIC_API_URL}`,
-            lastModified: new Date().toISOString(),
-            changeFrequency: 'weekly',
+//     if (data?.length < 1) {
+//         return [{
+//             url: `${process.env.NEXT_PUBLIC_API_URL}`,
+//             lastModified: new Date().toISOString(),
+//             changeFrequency: 'weekly',
+//             priority: 1,
+//         }];
+//     }
+
+//     const sites = data.map(site => ({
+//         url: site.url,
+//         lastModified: site.updatedAt.toISOString(),
+//         changeFrequency: 'weekly',
+//         priority: 1,
+//     }));
+
+//     return sites;
+// }
+
+export default function sitemap() {
+    return [
+        {
+            url: 'https://maricelascleaningmagnificence.com',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
             priority: 1,
-        }];
-    }
-
-    const sites = data.map(site => ({
-        url: site.url,
-        lastModified: site.updatedAt.toISOString(),
-        changeFrequency: 'weekly',
-        priority: 1,
-    }));
-
-    return sites;
+        }
+    ]
 }
