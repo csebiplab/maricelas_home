6d2a11586ce553a7069bccb5be0b77afe44453d5
Oliulla <razasm29@gmail.com>
2024-08-02 22:37:08 +0600

    remove gfont

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,4 +1,4 @@
-import { Open_Sans, Roboto_Slab } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import { Suspense } from "react";
 import "../globals.css";
 import RoutingProgressBar from "@/components/common/RoutingProgressBar";
@@ -7,7 +7,7 @@ import ScrollToTopComponent from "@/components/common/ScrollToTop";
 import { NavbarHeader } from "@/components/__layouts/Headers/Navbar";
 
 
-const roboto = Roboto_Slab({ subsets: ['latin'], display: 'swap', adjustFontFallback: false });
+// const roboto = Roboto_Slab({ subsets: ['latin'], display: 'swap', adjustFontFallback: false });
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export async function generateMetadata() {
@@ -79,13 +79,13 @@ function extractGoogleConsoleKey(verificationUrl) {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className || openSans.className}>
+      <body className={openSans.className}>
         <Suspense>
           <RoutingProgressBar />
         </Suspense>
         <>
-          
-          <NavbarHeader/>
+
+          <NavbarHeader />
         </>
         {children}
         <>
