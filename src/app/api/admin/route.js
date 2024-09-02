1db5ea1d59d23f53d7509ce43cff8309893a57be
Oliulla <razasm29@gmail.com>
2024-09-02 22:36:20 +0600

    done admin api

--- src/app/api/admin/route.js
+++ src/app/api/admin/route.js
@@ -1,66 +1,74 @@
 import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
-import dbConnect from "@/lib/db.connect";
-import Admin from "@/models/admin";
+import connectMongoDB from "@/lib/db";
+import AdminModel from "@/models/admin.model";
 
-// Temp api for create one time admin profile
 
 /*
+// Temp api to create one time admin profile
 export async function POST(request) {
     try {
-        // Parse the incoming JSON data containing username and password
         const { username, password } = await request.json();
 
-        // Connect to MongoDB
-        await dbConnect();
+        await connectMongoDB();
 
-        // Check if admin with the same username already exists
-        const existingAdmin = await Admin.findOne({ username });
+        const existingAdmin = await AdminModel.findOne({ username });
         if (existingAdmin) {
             return NextResponse.json(
                 { error: "Admin with this username already exists" },
-                { status: 400 } // Bad Request status code
+                { status: 400 }
             );
         }
 
-        // Hash the password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        // Create a new admin
-        await Admin.create({ username, password: hashedPassword });
+        await AdminModel.create({ username, password: hashedPassword });
 
         return NextResponse.json(
-            { message: "Admin created successfully" },
-            { status: 201 } // Created status code
+            { message: "Request Success", data: { username: username } },
+            { status: 201 }
         );
     } catch (error) {
         console.error("Error creating admin:", error);
         return NextResponse.json(
             { error: "Internal Server Error" },
-            { status: 500 } // Internal Server Error status code
+            { status: 500 }
         );
     }
-}*/
+}
+*/
 
-
-// Update admin profile
-export async function PUT(request) {
-    // Connect to MongoDB
-    await dbConnect();
+export async function PATCH(request) {
+    await connectMongoDB();
 
     // Start Mongoose session
     const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
-        const { currentUserName, newUserName, currentPassword, newPassword } = await request.json();
+        const updateField = await request.json();
 
-        // Find the admin by currentUserName
-        const admin = await Admin.findOne({ username: currentUserName }).session(session);
+        const { currentUserName, newUserName, currentPassword, newPassword } = updateField ?? {}
 
-        // If admin is not found, return an error
+        if (!currentUserName) {
+            return NextResponse.json(
+                { error: "Please provide current user name" },
+                { status: 401 }
+            );
+        }
+
+        if (!currentPassword) {
+            return NextResponse.json(
+                { error: "Please provide current password" },
+                { status: 401 }
+            );
+        }
+
+
+        const admin = await AdminModel.findOne({ username: currentUserName }).session(session);
+
         if (!admin) {
             return NextResponse.json(
                 { error: "Invalid username" },
@@ -68,10 +76,8 @@ export async function PUT(request) {
             );
         }
 
-        // Compare the provided currentPassword with the hashed password stored in the database
         const passwordMatch = await bcrypt.compare(currentPassword, admin.password);
 
-        // If passwords don't match, return an error
         if (!passwordMatch) {
             return NextResponse.json(
                 { error: "Invalid password" },
@@ -79,12 +85,10 @@ export async function PUT(request) {
             );
         }
 
-        // Update admin's username if newUserName is provided
         if (newUserName) {
             admin.username = newUserName;
         }
 
-        // Hash the new password if newPassword is provided
         if (newPassword) {
             const saltRounds = 10;
             const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
@@ -98,7 +102,7 @@ export async function PUT(request) {
         await session.commitTransaction();
 
         return NextResponse.json(
-            { message: "Updated successfully", admin: { userName: newUserName || currentUserName } },
+            { message: "Request Success", admin: { userName: newUserName || currentUserName } },
             { status: 200 }
         );
     } catch (error) {
