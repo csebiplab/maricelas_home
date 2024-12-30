5bed859b24e97b01af634ae9ec80ae7ce5e0d136
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-31 01:33:32 +0600

    benefits section add

--- src/components/__shared_one_time/HomeSections/Benefits/Benefits.jsx
+++ src/components/__shared_one_time/HomeSections/Benefits/Benefits.jsx
@@ -6,33 +6,33 @@ import { headingIconText } from '@/utils/heading-text';
 const ourRenovatorData1 = [
   {
     id: 1,
-    title: "Skipping subfloor preparation:",
-    desc: "A smooth, level subfloor is essential. Always clean, level, and dry the subfloor before starting. One of the most critical steps is ensuring your subfloor is clean, dry, and level. Failing to address imperfections or debris can",
+    title: "Reliable and Trustworthy",
+    desc: "Our team consists of thoroughly vetted professionals committed to delivering high-quality service with integrity. You can trust us to care for your home or business",
   },
   {
     id: 2,
-    title: "Ignoring acclimation:",
-    desc: "Vinyl planks need time to acclimate to the room's temperature and humidity. Skipping this step can lead to warping and gaps. Allow the planks to sit in the room for at least 48 hours before installation.",
+    title: "Customized Cleaning Plans",
+    desc: "Our cleaning professionals understand that every space is unique. We tailor our cleaning plans to your needs, we don't overlook any detail.",
   },
   {
     id: 3,
-    title: "Incorrect measuring and cutting:",
-    desc: "measuring accurately is crucial to avoid waste and ensure a tight fit. Skipping this layer or using an unsuitable underlayment can lead to premature wear and diminished comfort.",
+    title: "Time and stress saver",
+    desc: "Hiring us allows you to focus on what matters most while we take care of the cleaning. Save time and energy by leaving the dirty work to us.",
   },
   {
     id: 4,
-    title: "Not staggering planks: ",
-    desc: "Staggering planks create a natural look and strengthen the floor's structure. Aligning planks uniformly can weaken the installation and make it look artificial. Aim for at least a 6-inch stagger for visual appeal and stability. ",
+    title: "Flexible Scheduling",
+    desc: "Whether you need weekly, biweekly, monthly, or one-time services, we offer flexible scheduling to fit your busy lifestyle.",
   },
   {
     id: 5,
-    title: "Using the wrong adhesive (if applicable):",
-    desc: "For glue-down vinyl planks, using incompatible adhesive can cause planks to lift. Always use the adhesive recommended by the manufacturer to ensure a secure bond.",
+    title: "Experienced Professionals",
+    desc: "Our cleaning experts bring years of experience and skill, ensuring top-notch results every time.",
   },
   {
     id: 6,
-    title: "Using the wrong adhesive (if applicable):",
-    desc: "For glue-down vinyl planks, using incompatible adhesive can cause planks to lift. Always use the adhesive recommended by the manufacturer to ensure a secure bond."
+    title: "Consistent Quality",
+    desc: "Our expert cleaners deliver reliable, high-quality cleaning services every visit. You can enjoy a consistently clean and refreshed space."
   },
 ];
 
@@ -42,21 +42,22 @@ const Benefits = () => {
       <div data-aos="zoom-in" className="container padding__top">
         <div className="flex flex-col items-center justify-center">
         <HeadingIcon text={headingIconText.benefits__headingIconText}/>
-          <h2 className="text-[25px] md:text-[27px] lg:text-[30px] xl:text-[33px] 2xl:text-[35px] 3xl:text-[38px] 4xl:text-[40px] 5xl:tex-[42px] font-bold text-black leading-tight 5xl:leading-normal mb-[10px] text-center md:text-left">
-          Common Mistakes When Installing Vinyl Plank Flooring
+          <h2 className="text-[25px] md:text-[27px] lg:text-[30px] xl:text-[33px] 2xl:text-[35px] 3xl:text-[38px] 4xl:text-[40px] 5xl:tex-[42px] font-bold text-black leading-tight 5xl:leading-normal mb-[10px] text-center">
+          Benefits of Working With Our Cleaning Services in Houston
           </h2>
+          <p className="text-base 4xl:text-[17px] 5xl:text-lg font-normal text-dark leading-[30px] 5xl:leading-[36px] text-center">Choosing Maricela's Cleaning Magnificence comes with several key advantages</p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-[18px] 2xl:gap-[21px] 3xl:gap-[24px] 4xl:gap-[27px] 5xl:gap-[30px] mt-[25px]">
           {ourRenovatorData1?.map((data) => {
             return (
               <div
                 key={data.id}
-                className="mistakes__cart__bg pt-[14px] pb-[21px] px-2"
+                className="benefits__cart__bg pt-[14px] pb-[21px] px-2"
               >
-                <p className="text-lg 5xl:text-[20px] text-center font-bold text-black leading-normal">
+                <p className="text-[20px] xl:text-[21px] 2xl:text-[22px] 3xl:text-[23px] 4xl:text-[24px] 5xl:tex5-[25px] text-center font-bold text-black leading-normal">
                   {data.title}
                 </p>
-                <p className="text-base 5xl:text-lg font-normal text-black leading-[35px] text-center">
+                <p className="text-base 4xl:text-[17px] 5xl:text-lg font-normal text-black leading-[35px] text-center">
                   {data.desc}
                 </p>
               </div>
@@ -66,20 +67,20 @@ const Benefits = () => {
 
         {/* second part  */}
         <div className="w-full flex flex-col md:flex-row justify-center mt-[18px] 2xl:mt-[21px] 3xl:mt-[24px] 4xl:mt-[27px] 5xl:mt-[30px] gap-[18px] 2xl:gap-[21px] 3xl:gap-[24px] 4xl:gap-[27px] 5xl:gap-[30px] mt-[25px]">
-          <div className="w-full md:w-1/3 mistakes__cart__bg pt-[14px] pb-[21px] px-2">
-            <p className="text-lg 5xl:text-[20px] text-center font-bold text-black leading-normal">
-            Not following manufacturer's instructions:
+          <div className="w-full md:w-1/3 benefits__cart__bg pt-[14px] pb-[21px] px-2">
+            <p className="text-[20px] xl:text-[21px] 2xl:text-[22px] 3xl:text-[23px] 4xl:text-[24px] 5xl:tex5-[25px] text-center font-bold text-black leading-normal">
+            Healthier Environment
             </p>
-            <p className="text-base 5xl:text-lg font-normal text-black leading-[35px] text-center">
-            Ignoring these instructions, such as the recommended adhesive type or installation method, can affect the warranty and performance of your flooring.
+            <p className="text-base 4xl:text-[17px] 5xl:text-lg font-normal text-black leading-[35px] text-center">
+            A clean space always promotes a healthier lifestyle. Our deep cleaning services in Houston reduce dust, allergens, and germs, making your home or office safer for everyone.
             </p>
           </div>
-          <div className="w-full md:w-1/3 mistakes__cart__bg pt-[14px] pb-[21px] px-2">
-            <p className="text-lg 5xl:text-[20px] text-center font-bold text-black leading-normal">
-            Rushing the installation:
+          <div className="w-full md:w-1/3 benefits__cart__bg pt-[14px] pb-[21px] px-2">
+            <p className="text-[20px] xl:text-[21px] 2xl:text-[22px] 3xl:text-[23px] 4xl:text-[24px] 5xl:tex5-[25px] text-center font-bold text-black leading-normal">
+            Satisfaction Guaranteed
             </p>
-            <p className="text-base 5xl:text-lg font-normal text-black leading-[35px] text-center">
-            Taking time is essential for achieving a professional-looking result. Rushing through the installation can lead to misaligned planks, uneven seams, or poorly finished edges.
+            <p className="text-base 4xl:text-[17px] 5xl:text-lg font-normal text-black leading-[35px] text-center">
+            Your happiness is our priority. If you're ever unsatisfied with our work, we'll make it right—because we're not happy until you are.
             </p>
           </div>
         </div>
